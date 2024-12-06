98ac3c1b41577894b5fc2f4b7df0f3f85b1b6686
Lillie <91358136+LillieH1000@users.noreply.github.com>
2024-12-06 18:14:10 -0500

    Refactor to mediasessionservice

--- app/src/main/java/h/lillie/ytplayer/MainService.kt
+++ app/src/main/java/h/lillie/ytplayer/MainService.kt
@@ -1,56 +1,41 @@
 package h.lillie.ytplayer
 
-import android.accessibilityservice.AccessibilityService
-import android.content.ComponentName
-import android.content.Intent
-import android.media.MediaMetadata
-import android.media.session.MediaSessionManager
 import android.os.Handler
 import android.os.Looper
-import android.provider.Settings
-import android.service.notification.NotificationListenerService
-import android.util.Log
-import android.view.accessibility.AccessibilityEvent
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.session.MediaSession
+import androidx.media3.session.MediaSessionService
 
-class NotificationService : NotificationListenerService()
+class MainService : MediaSessionService() {
+    private lateinit var playerHandler: Handler
+    private var playerSession: MediaSession? = null
 
-class MainService : AccessibilityService() {
-    private lateinit var mainHandler: Handler
+    override fun onCreate() {
+        super.onCreate()
 
-    override fun onServiceConnected() {
-        super.onServiceConnected()
+        val exoPlayer = ExoPlayer.Builder(this).build()
+        playerSession = MediaSession.Builder(this, exoPlayer).build()
 
-        val component = ComponentName(this@MainService, NotificationService::class.java)
-        val listener = Settings.Secure.getString(contentResolver, "enabled_notification_listeners")
-        if (listener == null || !listener.contains(component.flattenToString())) {
-            val intent = Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS")
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            startActivity(intent)
+        playerHandler = Handler(Looper.getMainLooper())
+        playerHandler.post(playerTask)
+    }
+
+    override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? {
+        return playerSession
+    }
+
+    override fun onDestroy() {
+        playerSession?.run {
+            player.release()
+            release()
+            playerSession = null
         }
-
-        mainHandler = Handler(Looper.getMainLooper())
-        mainHandler.post(mainTask)
+        super.onDestroy()
     }
 
-    override fun onAccessibilityEvent(event: AccessibilityEvent?) {
-    }
-
-    override fun onInterrupt() {
-    }
-
-    private val mainTask = object : Runnable {
+    private val playerTask = object : Runnable {
         override fun run() {
-            val component = ComponentName(this@MainService, NotificationService::class.java)
-            val listener = Settings.Secure.getString(contentResolver, "enabled_notification_listeners")
-            if (listener != null && listener.contains(component.flattenToString())) {
-                val mediaSessionManager = getSystemService(MEDIA_SESSION_SERVICE) as MediaSessionManager
-                val activeSessions = mediaSessionManager.getActiveSessions(component)
-                activeSessions.forEach {
-                    Log.d("Media Playback Info", it.playbackInfo.toString())
-                    Log.d("Media Metadata", it.metadata!!.getString(MediaMetadata.METADATA_KEY_TITLE))
-                }
-            }
-            mainHandler.postDelayed(this, 1000)
+            playerHandler.postDelayed(this, 1000)
         }
     }
 }
