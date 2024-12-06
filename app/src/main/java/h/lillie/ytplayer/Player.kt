a59563627cfe4fdf196a6f27171f14351a2b3d2a
Lillie <91358136+LillieH1000@users.noreply.github.com>
2024-12-06 18:33:06 -0500

    Add player activity

---
+++ app/src/main/java/h/lillie/ytplayer/Player.kt
@@ -0,0 +1,27 @@
+package h.lillie.ytplayer
+
+import android.content.ComponentName
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.media3.session.MediaController
+import androidx.media3.session.SessionToken
+import com.google.common.util.concurrent.ListenableFuture
+import com.google.common.util.concurrent.MoreExecutors
+
+class Player : AppCompatActivity() {
+    private lateinit var playerControllerFuture: ListenableFuture<MediaController>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val sessionToken = SessionToken(this, ComponentName(this, PlayerService::class.java))
+        playerControllerFuture = MediaController.Builder(this, sessionToken).buildAsync()
+        playerControllerFuture.addListener({
+        }, MoreExecutors.directExecutor())
+    }
+
+    override fun onStop() {
+        super.onStop()
+        MediaController.releaseFuture(playerControllerFuture)
+    }
+}
