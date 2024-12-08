648fc62ae953f50b26afaf3aee35cce85dd60bc5
Lillie <91358136+LillieH1000@users.noreply.github.com>
2024-12-07 20:15:29 -0500

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         minSdk = 33
         // noinspection OldTargetApi, EditedTargetSdkVersion
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     buildTypes {
