1777d45c7baf4ab8b1422792c0722a2f5592b733
Lillie <91358136+LillieH1000@users.noreply.github.com>
2024-12-16 14:33:29 -0500

    Use custom cast options provider

---
+++ app/src/main/java/h/lillie/ytplayer/CastOptionsProvider.kt
@@ -0,0 +1,31 @@
+package h.lillie.ytplayer
+
+import android.content.Context
+import androidx.annotation.OptIn
+import androidx.media3.cast.DefaultCastOptionsProvider
+import androidx.media3.common.util.UnstableApi
+import com.google.android.gms.cast.framework.CastOptions
+import com.google.android.gms.cast.framework.OptionsProvider
+import com.google.android.gms.cast.framework.SessionProvider
+import com.google.android.gms.cast.framework.media.CastMediaOptions
+import java.util.Collections
+
+class CastOptionsProvider : OptionsProvider {
+    @OptIn(UnstableApi::class)
+    override fun getCastOptions(p0: Context): CastOptions {
+        return CastOptions.Builder()
+            .setCastMediaOptions(CastMediaOptions.Builder()
+                .setMediaSessionEnabled(false)
+                .setNotificationOptions(null)
+                .build())
+            .setEnableReconnectionService(false)
+            .setReceiverApplicationId(DefaultCastOptionsProvider.APP_ID_DEFAULT_RECEIVER_WITH_DRM)
+            .setResumeSavedSession(false)
+            .setStopReceiverApplicationWhenEndingSession(true)
+            .build()
+    }
+
+    override fun getAdditionalSessionProviders(p0: Context): MutableList<SessionProvider>? {
+        return Collections.emptyList()
+    }
+}
