aec23121d09405e174d019d92652849685464eec
Lillie <91358136+LillieH1000@users.noreply.github.com>
2024-12-17 20:45:13 -0500

    Actually change to my custom receiver application

--- app/src/main/java/h/lillie/ytplayer/CastOptionsProvider.kt
+++ app/src/main/java/h/lillie/ytplayer/CastOptionsProvider.kt
@@ -1,16 +1,12 @@
 package h.lillie.ytplayer
 
 import android.content.Context
-import androidx.annotation.OptIn
-import androidx.media3.cast.DefaultCastOptionsProvider
-import androidx.media3.common.util.UnstableApi
 import com.google.android.gms.cast.framework.CastOptions
 import com.google.android.gms.cast.framework.OptionsProvider
 import com.google.android.gms.cast.framework.SessionProvider
 import com.google.android.gms.cast.framework.media.CastMediaOptions
 import java.util.Collections
 
-@OptIn(UnstableApi::class)
 class CastOptionsProvider : OptionsProvider {
     override fun getCastOptions(p0: Context): CastOptions {
         return CastOptions.Builder()
@@ -19,7 +15,7 @@ class CastOptionsProvider : OptionsProvider {
                 .setNotificationOptions(null)
                 .build())
             .setEnableReconnectionService(false)
-            .setReceiverApplicationId(DefaultCastOptionsProvider.APP_ID_DEFAULT_RECEIVER_WITH_DRM)
+            .setReceiverApplicationId("FFE726D7")
             .setResumeSavedSession(false)
             .setStopReceiverApplicationWhenEndingSession(true)
             .build()
