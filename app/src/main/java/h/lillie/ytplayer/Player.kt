fe1f522f2ca17ba7f7acdb2be7441dcd95fb9d44
Lillie <91358136+LillieH1000@users.noreply.github.com>
2025-01-24 22:18:31 -0500

    Revert "Disable playerview focus on android tv"
    This reverts commit e5c50a8481a3d920ca51a57f72f6058fd9343d5d.

--- app/src/main/java/h/lillie/ytplayer/Player.kt
+++ app/src/main/java/h/lillie/ytplayer/Player.kt
@@ -299,14 +299,12 @@ class Player : AppCompatActivity(), Player.Listener {
 
                     val playerView: PlayerView = findViewById(R.id.playerView)
                     playerView.player = playerController
-                    if (!Application.androidTVDevice) {
-                        playerView.focusable = FOCUSABLE
-                        playerView.isFocusableInTouchMode = true
-                        playerView.requestFocus()
-                        playerView.setOnFocusChangeListener { v, hasFocus ->
-                            if (!hasFocus) {
-                                v.requestFocus()
-                            }
+                    playerView.focusable = FOCUSABLE
+                    playerView.isFocusableInTouchMode = true
+                    playerView.requestFocus()
+                    playerView.setOnFocusChangeListener { v, hasFocus ->
+                        if (!hasFocus) {
+                            v.requestFocus()
                         }
                     }
 
