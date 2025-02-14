fc5220d4aa708ccbfe385ef2933c44728bdbff4d
Lillie <91358136+LillieH1000@users.noreply.github.com>
2025-02-14 17:56:47 -0500

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         minSdk = 26
         // noinspection OldTargetApi, EditedTargetSdkVersion
         targetSdk = 34
-        versionCode = 27
-        versionName = "1.3.06"
+        versionCode = 28
+        versionName = "1.3.10"
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a", "x86_64", "x86")
         }
