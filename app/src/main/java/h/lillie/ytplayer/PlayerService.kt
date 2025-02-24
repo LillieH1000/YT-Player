266d90957f98d07c806074e2d41a163bcb465c4a
Lillie <91358136+LillieH1000@users.noreply.github.com>
2025-02-24 13:33:20 -0500

    Add caching support

--- app/src/main/java/h/lillie/ytplayer/PlayerService.kt
+++ app/src/main/java/h/lillie/ytplayer/PlayerService.kt
@@ -22,8 +22,12 @@ import androidx.media3.common.MimeTypes
 import androidx.media3.common.PlaybackParameters
 import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
+import androidx.media3.database.StandaloneDatabaseProvider
 import androidx.media3.datasource.DataSource
 import androidx.media3.datasource.DefaultDataSource
+import androidx.media3.datasource.cache.CacheDataSource
+import androidx.media3.datasource.cache.LeastRecentlyUsedCacheEvictor
+import androidx.media3.datasource.cache.SimpleCache
 import androidx.media3.exoplayer.DefaultRenderersFactory
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.exoplayer.hls.HlsMediaSource
@@ -42,6 +46,7 @@ import com.google.common.util.concurrent.Futures
 import com.google.common.util.concurrent.ListenableFuture
 import com.google.common.util.concurrent.MoreExecutors
 import org.json.JSONArray
+import java.io.File
 import java.text.DecimalFormat
 
 @OptIn(UnstableApi::class)
@@ -217,7 +222,12 @@ class PlayerService: MediaSessionService(), MediaSession.Callback {
                 }
 
                 val dataSourceFactory: DataSource.Factory = DefaultDataSource.Factory(this@PlayerService)
-                val hlsSource: MediaSource = HlsMediaSource.Factory(dataSourceFactory)
+                val simpleCache = SimpleCache(File(cacheDir, "media"), LeastRecentlyUsedCacheEvictor(200 * 1024 * 1024), StandaloneDatabaseProvider(this@PlayerService))
+                val cacheDataSource: CacheDataSource.Factory = CacheDataSource.Factory()
+                    .setUpstreamDataSourceFactory(dataSourceFactory)
+                    .setCache(simpleCache)
+
+                val hlsSource: MediaSource = HlsMediaSource.Factory(cacheDataSource)
                     .setAllowChunklessPreparation(false)
                     .createMediaSource(playerMediaItem.build())
 
