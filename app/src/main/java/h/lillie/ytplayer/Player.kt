251218a67e5cf955eb1f3a52e0e34f77f688bf61
Lillie <91358136+LillieH1000@users.noreply.github.com>
2025-02-24 14:02:32 -0500

    Revert "Add toast for invalid url"
    This reverts commit dc009bc41b75dcc62c28f8dae23606b39991993b.

--- app/src/main/java/h/lillie/ytplayer/Player.kt
+++ app/src/main/java/h/lillie/ytplayer/Player.kt
@@ -341,8 +341,6 @@ class Player: AppCompatActivity(), Player.Listener {
                     sendBroadcast(broadcastIntent)
                 }, MoreExecutors.directExecutor())
             }
-        } else {
-            Toast.makeText(this, "Invalid Url", Toast.LENGTH_LONG).show()
         }
     }
 
