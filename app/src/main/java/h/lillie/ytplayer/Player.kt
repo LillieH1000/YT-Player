e647ff889c89bbe36496466fb1783902160a3e90
Lillie <91358136+LillieH1000@users.noreply.github.com>
2025-02-28 17:43:18 -0500

    Fix padding for system ui elements

--- app/src/main/java/h/lillie/ytplayer/Player.kt
+++ app/src/main/java/h/lillie/ytplayer/Player.kt
@@ -22,7 +22,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
@@ -207,7 +210,11 @@ class Player: ComponentActivity(), Player.Listener {
                 }
             },
         )
-        Row(modifier = Modifier.fillMaxSize()) {
+        Row(modifier = Modifier
+            .fillMaxSize()
+            .navigationBarsPadding()
+            .statusBarsPadding()
+            .systemBarsPadding()) {
             Box(
                 modifier = Modifier
                     .fillMaxHeight()
