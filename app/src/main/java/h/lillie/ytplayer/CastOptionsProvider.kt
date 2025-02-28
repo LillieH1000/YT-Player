de07f04ec2bbe0a0b3da54748c498a8b7d128928
Lillie <91358136+LillieH1000@users.noreply.github.com>
2025-02-28 18:20:35 -0500

    Re-add v1 playerservice and cast provider

---
+++ app/src/main/java/h/lillie/ytplayer/CastOptionsProvider.kt
@@ -0,0 +1,27 @@
+package h.lillie.ytplayer
+
+import android.content.Context
+import com.google.android.gms.cast.framework.CastOptions
+import com.google.android.gms.cast.framework.OptionsProvider
+import com.google.android.gms.cast.framework.SessionProvider
+import com.google.android.gms.cast.framework.media.CastMediaOptions
+import java.util.Collections
+
+class CastOptionsProvider: OptionsProvider {
+    override fun getCastOptions(p0: Context): CastOptions {
+        return CastOptions.Builder()
+            .setCastMediaOptions(CastMediaOptions.Builder()
+                .setMediaSessionEnabled(false)
+                .setNotificationOptions(null)
+                .build())
+            .setEnableReconnectionService(false)
+            .setReceiverApplicationId("FFE726D7")
+            .setResumeSavedSession(false)
+            .setStopReceiverApplicationWhenEndingSession(true)
+            .build()
+    }
+
+    override fun getAdditionalSessionProviders(p0: Context): MutableList<SessionProvider>? {
+        return Collections.emptyList()
+    }
+}
--- app/src/main/java/h/lillie/ytplayer/PlayerService.kt
+++ app/src/main/java/h/lillie/ytplayer/PlayerService.kt
@@ -8,8 +8,15 @@ import android.content.IntentFilter
 import android.net.Uri
 import android.net.http.HttpEngine
 import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.os.ext.SdkExtensions
+import android.widget.Toast
 import androidx.annotation.OptIn
+import androidx.media3.cast.CastPlayer
+import androidx.media3.cast.DefaultMediaItemConverter
+import androidx.media3.cast.SessionAvailabilityListener
 import androidx.media3.common.C
 import androidx.media3.common.MediaItem
 import androidx.media3.common.MediaMetadata
@@ -17,21 +24,41 @@ import androidx.media3.common.MimeTypes
 import androidx.media3.common.PlaybackParameters
 import androidx.media3.common.Player
 import androidx.media3.common.util.UnstableApi
+import androidx.media3.database.StandaloneDatabaseProvider
 import androidx.media3.datasource.DefaultDataSource
 import androidx.media3.datasource.HttpEngineDataSource
+import androidx.media3.datasource.cache.CacheDataSource
+import androidx.media3.datasource.cache.LeastRecentlyUsedCacheEvictor
+import androidx.media3.datasource.cache.SimpleCache
 import androidx.media3.exoplayer.DefaultRenderersFactory
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.exoplayer.hls.HlsMediaSource
 import androidx.media3.exoplayer.source.MediaSource
 import androidx.media3.exoplayer.trackselection.DefaultTrackSelector
+import androidx.media3.session.CommandButton
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
+import androidx.media3.session.SessionCommand
+import androidx.media3.session.SessionResult
+import com.google.android.gms.cast.framework.CastContext
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.common.collect.ImmutableList
+import com.google.common.util.concurrent.Futures
+import com.google.common.util.concurrent.ListenableFuture
 import com.google.common.util.concurrent.MoreExecutors
+import org.json.JSONArray
+import java.io.File
+import java.text.DecimalFormat
 
 @OptIn(UnstableApi::class)
 @SuppressLint("UnspecifiedRegisterReceiverFlag")
 class PlayerService: MediaSessionService(), MediaSession.Callback {
     private lateinit var exoPlayer: ExoPlayer
+    private lateinit var playerHandler: Handler
+    private lateinit var playerCache: SimpleCache
+    private val backCommand = SessionCommand("back", Bundle.EMPTY)
+    private val forwardCommand = SessionCommand("forward", Bundle.EMPTY)
     private var playerSession: MediaSession? = null
 
     override fun onCreate() {
@@ -53,8 +80,21 @@ class PlayerService: MediaSessionService(), MediaSession.Callback {
             .setSeekForwardIncrementMs(10000)
             .build()
 
+        val backButton: CommandButton = CommandButton.Builder()
+                .setDisplayName("Seek Back")
+                .setIconResId(androidx.media3.session.R.drawable.media3_icon_skip_back_10)
+                .setSessionCommand(backCommand)
+                .build()
+
+        val forwardButton: CommandButton = CommandButton.Builder()
+            .setDisplayName("Seek Forward")
+            .setIconResId(androidx.media3.session.R.drawable.media3_icon_skip_forward_10)
+            .setSessionCommand(forwardCommand)
+            .build()
+
         playerSession = MediaSession.Builder(this, exoPlayer)
             .setCallback(this)
+            .setCustomLayout(ImmutableList.of(backButton, forwardButton))
             .build()
 
         if (Build.VERSION.SDK_INT <= 32) {
@@ -62,6 +102,57 @@ class PlayerService: MediaSessionService(), MediaSession.Callback {
         } else {
             registerReceiver(playerBroadcastReceiver, IntentFilter("h.lillie.ytplayer.info"), RECEIVER_NOT_EXPORTED)
         }
+
+        val playServiceAvailable: Int = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(this)
+        if (playServiceAvailable == ConnectionResult.SUCCESS) {
+            try {
+                CastContext.getSharedInstance(this, MoreExecutors.directExecutor()).result
+                Application.castExists = true
+            } catch (_: RuntimeException) {
+                Application.castExists = false
+            }
+        } else {
+            Application.castExists = false
+        }
+
+        if (Application.castExists) {
+            val castPlayer = CastPlayer(CastContext.getSharedInstance(this, MoreExecutors.directExecutor()).result, DefaultMediaItemConverter(), 10000, 10000)
+            castPlayer.setSessionAvailabilityListener(object : SessionAvailabilityListener {
+                override fun onCastSessionAvailable() {
+                    Application.castActive = true
+                    exoPlayer.stop()
+                    playerSession?.player = castPlayer
+
+                    val playerMediaMetadata: MediaMetadata = MediaMetadata.Builder()
+                        .setTitle(Application.title)
+                        .setArtist(Application.author)
+                        .setArtworkUri(Uri.parse(Application.artwork))
+                        .build()
+
+                    val playerMediaItem: MediaItem = MediaItem.Builder()
+                        .setMimeType(MimeTypes.AUDIO_MP4)
+                        .setMediaMetadata(playerMediaMetadata)
+                        .setUri(Uri.parse(Application.audioUrl))
+                        .build()
+
+                    castPlayer.setMediaItem(playerMediaItem, exoPlayer.currentPosition)
+                    castPlayer.playWhenReady = true
+                    castPlayer.prepare()
+                }
+
+                override fun onCastSessionUnavailable() {
+                    Application.castActive = false
+                    castPlayer.stop()
+                    playerSession?.player = exoPlayer
+                    exoPlayer.seekTo(castPlayer.currentPosition)
+                    exoPlayer.playWhenReady = true
+                    exoPlayer.prepare()
+                }
+            })
+        }
+
+        playerHandler = Handler(Looper.getMainLooper())
+        playerHandler.post(playerTask)
     }
 
     override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? {
@@ -70,12 +161,42 @@ class PlayerService: MediaSessionService(), MediaSession.Callback {
 
     override fun onDestroy() {
         playerSession?.run {
+            playerHandler.removeCallbacksAndMessages(null)
             unregisterReceiver(playerBroadcastReceiver)
+            playerCache.release()
             player.release()
             release()
             playerSession = null
         }
         super.onDestroy()
+    }
+
+    override fun onConnect(session: MediaSession, controller: MediaSession.ControllerInfo): MediaSession.ConnectionResult {
+        return MediaSession.ConnectionResult.AcceptedResultBuilder(session)
+            .setAvailablePlayerCommands(MediaSession.ConnectionResult.DEFAULT_PLAYER_COMMANDS.buildUpon()
+                .remove(Player.COMMAND_SEEK_TO_PREVIOUS_MEDIA_ITEM)
+                .remove(Player.COMMAND_SEEK_TO_PREVIOUS)
+                .remove(Player.COMMAND_SEEK_TO_NEXT_MEDIA_ITEM)
+                .remove(Player.COMMAND_SEEK_TO_NEXT)
+                .build()
+            )
+            .setAvailableSessionCommands(MediaSession.ConnectionResult.DEFAULT_SESSION_COMMANDS.buildUpon()
+                .add(backCommand)
+                .add(forwardCommand)
+                .build()
+            ).build()
+    }
+
+    override fun onCustomCommand(session: MediaSession, controller: MediaSession.ControllerInfo, customCommand: SessionCommand, args: Bundle): ListenableFuture<SessionResult> {
+        if (customCommand.customAction == "back") {
+            playerSession?.player?.seekBack()
+            return Futures.immediateFuture(SessionResult(SessionResult.RESULT_SUCCESS))
+        }
+        if (customCommand.customAction == "forward") {
+            playerSession?.player?.seekForward()
+            return Futures.immediateFuture(SessionResult(SessionResult.RESULT_SUCCESS))
+        }
+        return super.onCustomCommand(session, controller, customCommand, args)
     }
 
     private val playerBroadcastReceiver = object: BroadcastReceiver() {
@@ -92,24 +213,60 @@ class PlayerService: MediaSessionService(), MediaSession.Callback {
                     .setMediaMetadata(playerMediaMetadata)
                     .setUri(Uri.parse(Application.hlsUrl))
 
+                if (Application.enCaptions != "null") {
+                    val enPlayerCaptions: MediaItem.SubtitleConfiguration = MediaItem.SubtitleConfiguration.Builder(Uri.parse(Application.enCaptions))
+                        .setSelectionFlags(C.SELECTION_FLAG_DEFAULT)
+                        .setMimeType(MimeTypes.TEXT_VTT)
+                        .setLanguage("en")
+                        .build()
+
+                    playerMediaItem.setSubtitleConfigurations(listOf(enPlayerCaptions))
+                }
+
+                if (this@PlayerService::playerCache.isInitialized) {
+                    playerCache.release()
+                }
+                playerCache = SimpleCache(File(cacheDir, "media"), LeastRecentlyUsedCacheEvictor(200 * 1024 * 1024), StandaloneDatabaseProvider(this@PlayerService))
+                val cacheDataSource: CacheDataSource.Factory = CacheDataSource.Factory().setCache(playerCache)
+
                 if (Build.VERSION.SDK_INT >= 34 && SdkExtensions.getExtensionVersion(Build.VERSION_CODES.S) >= 7) {
                     val httpEngine: HttpEngine = HttpEngine.Builder(this@PlayerService)
                         .setEnableHttp2(true)
                         .build()
 
                     val httpEngineDataSource: HttpEngineDataSource.Factory = HttpEngineDataSource.Factory(httpEngine, MoreExecutors.directExecutor())
-                    val hlsSource: MediaSource = HlsMediaSource.Factory(httpEngineDataSource)
-                        .setAllowChunklessPreparation(false)
-                        .createMediaSource(playerMediaItem.build())
+                    if (!Application.live) {
+                        cacheDataSource.setUpstreamDataSourceFactory(httpEngineDataSource)
 
-                    exoPlayer.setMediaSource(hlsSource)
+                        val hlsSource: MediaSource = HlsMediaSource.Factory(cacheDataSource)
+                            .setAllowChunklessPreparation(false)
+                            .createMediaSource(playerMediaItem.build())
+
+                        exoPlayer.setMediaSource(hlsSource)
+                    } else {
+                        val hlsSource: MediaSource = HlsMediaSource.Factory(httpEngineDataSource)
+                            .setAllowChunklessPreparation(false)
+                            .createMediaSource(playerMediaItem.build())
+
+                        exoPlayer.setMediaSource(hlsSource)
+                    }
                 } else {
                     val defaultDataSource: DefaultDataSource.Factory = DefaultDataSource.Factory(this@PlayerService)
-                    val hlsSource: MediaSource = HlsMediaSource.Factory(defaultDataSource)
-                        .setAllowChunklessPreparation(false)
-                        .createMediaSource(playerMediaItem.build())
+                    if (!Application.live) {
+                        cacheDataSource.setUpstreamDataSourceFactory(defaultDataSource)
 
-                    exoPlayer.setMediaSource(hlsSource)
+                        val hlsSource: MediaSource = HlsMediaSource.Factory(cacheDataSource)
+                            .setAllowChunklessPreparation(false)
+                            .createMediaSource(playerMediaItem.build())
+
+                        exoPlayer.setMediaSource(hlsSource)
+                    } else {
+                        val hlsSource: MediaSource = HlsMediaSource.Factory(defaultDataSource)
+                            .setAllowChunklessPreparation(false)
+                            .createMediaSource(playerMediaItem.build())
+
+                        exoPlayer.setMediaSource(hlsSource)
+                    }
                 }
 
                 exoPlayer.repeatMode = Player.REPEAT_MODE_OFF
@@ -120,6 +277,28 @@ class PlayerService: MediaSessionService(), MediaSession.Callback {
                 exoPlayer.playWhenReady = true
                 exoPlayer.prepare()
             }
+        }
+    }
+
+    private val playerTask = object: Runnable {
+        override fun run() {
+            val sponsorBlock: JSONArray? = Application.sponsorBlock
+            if (sponsorBlock != null && !Application.live) {
+                for (i in 0 until sponsorBlock.length()) {
+                    val decimalFormat = DecimalFormat("#.###")
+
+                    val segment: JSONArray = sponsorBlock.getJSONObject(i).getJSONArray("segment")
+                    val position: Double = decimalFormat.format(playerSession?.player!!.currentPosition / 1000.0).toDouble()
+                    val segment0: Double = decimalFormat.format(segment[0]).toDouble()
+                    val segment1: Double = decimalFormat.format(segment[1]).toDouble()
+
+                    if (position >= segment0 && position < segment1) {
+                        playerSession?.player!!.seekTo(decimalFormat.format(segment1 * 1000.0).toLong())
+                        Toast.makeText(this@PlayerService, "Sponsor Skipped", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+            playerHandler.postDelayed(this, 1000)
         }
     }
 }
