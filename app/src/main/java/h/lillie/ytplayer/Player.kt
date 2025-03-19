33879e0f572ec9f9b0d50f898655a7904e82cbf3
Lillie <91358136+LillieH1000@users.noreply.github.com>
2025-03-19 17:46:47 -0400

    Add circle progress indicator for player buffering

--- app/src/main/java/h/lillie/ytplayer/Player.kt
+++ app/src/main/java/h/lillie/ytplayer/Player.kt
@@ -41,6 +41,7 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -58,7 +59,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.colorResource
@@ -337,29 +337,31 @@ class Player: ComponentActivity(), Player.Listener {
                         }
                     }
                 ) {
-                    Icon(
-                        modifier = if (isPlaying == 0) {
-                            Modifier.size(50.dp).alpha(0f)
-                        } else {
-                            Modifier.size(50.dp).alpha(1f)
-                        },
-                        painter = when (isPlaying) {
-                            1 -> {
-                                painterResource(androidx.media3.session.R.drawable.media3_icon_skip_back)
-                            }
-                            2 -> {
-                                painterResource(androidx.media3.session.R.drawable.media3_icon_play)
-                            }
-                            3 -> {
-                                painterResource(androidx.media3.session.R.drawable.media3_icon_pause)
-                            }
-                            else -> {
-                                painterResource(R.drawable.empty)
-                            }
-                        },
-                        tint = colorResource(R.color.white),
-                        contentDescription = ""
-                    )
+                    if (isPlaying == 1) {
+                        CircularProgressIndicator(
+                            modifier = Modifier.size(30.dp),
+                            strokeWidth = 3.dp,
+                            color = colorResource(R.color.white)
+                        )
+                    }
+                    if (isPlaying >= 2) {
+                        Icon(
+                            modifier = Modifier.size(50.dp),
+                            painter = when (isPlaying) {
+                                2 -> {
+                                    painterResource(androidx.media3.session.R.drawable.media3_icon_play)
+                                }
+                                3 -> {
+                                    painterResource(androidx.media3.session.R.drawable.media3_icon_pause)
+                                }
+                                else -> {
+                                    painterResource(androidx.media3.session.R.drawable.media3_icon_skip_back)
+                                }
+                            },
+                            tint = colorResource(R.color.white),
+                            contentDescription = ""
+                        )
+                    }
                 }
                 // Progress Slider
                 Slider(
@@ -755,13 +757,19 @@ class Player: ComponentActivity(), Player.Listener {
         override fun run() {
             val player: MediaController? = playerController.value
             if (player != null && player.mediaItemCount == 1) {
-                if (player.playbackState == Player.STATE_ENDED) {
-                    isPlaying.intValue = 1
-                } else {
-                    if (!player.isPlaying) {
-                        isPlaying.intValue = 2
-                    } else {
-                        isPlaying.intValue = 3
+                when (player.playbackState) {
+                    Player.STATE_BUFFERING -> {
+                        isPlaying.intValue = 1
+                    }
+                    Player.STATE_ENDED -> {
+                        isPlaying.intValue = 4
+                    }
+                    else -> {
+                        if (!player.isPlaying) {
+                            isPlaying.intValue = 2
+                        } else {
+                            isPlaying.intValue = 3
+                        }
                     }
                 }
 
