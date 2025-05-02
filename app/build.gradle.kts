d2b3dcd62038a6e42fba1283b8a2ec3db325c2c7
Lillie <91358136+LillieH1000@users.noreply.github.com>
2025-05-01 20:45:38 -0400

    Bump depends version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "h.lillie.ytplayer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 55
-        versionName = "2.1.05"
+        versionCode = 57
+        versionName = "2.1.06"
         ndk {
             abiFilters += listOf("arm64-v8a", "x86_64")
         }
