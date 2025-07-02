f5a78895db3b496953e920b9c06567b1c5c5dbdb
Lillie <91358136+LillieH1000@users.noreply.github.com>
2025-07-01 21:01:41 -0400

    Bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "h.lillie.ytplayer"
         minSdk = 26
         targetSdk = 36
-        versionCode = 68
-        versionName = "2.1.20"
+        versionCode = 69
+        versionName = "2.1.21"
         ndk {
             abiFilters += listOf("arm64-v8a", "x86_64")
         }
