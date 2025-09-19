cb157825b0a6f5229fc37f3a6dca2a222f3f2365
Lillie <91358136+LillieH1000@users.noreply.github.com>
2025-09-19 19:34:31 -0400

    Update lifecycle depends

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         applicationId = "h.lillie.ytplayer"
         minSdk = 25
         targetSdk = 36
-        versionCode = 120
+        versionCode = 125
         versionName = "3.2.0"
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a", "x86_64", "x86")
