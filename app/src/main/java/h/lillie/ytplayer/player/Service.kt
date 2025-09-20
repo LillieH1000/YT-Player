fb7911974408150bd8525d9c27cbdda881df4dfa
Lillie <91358136+LillieH1000@users.noreply.github.com>
2025-09-20 14:28:22 -0400

    Set preferred audio languages to track selector

--- app/src/main/java/h/lillie/ytplayer/player/Service.kt
+++ app/src/main/java/h/lillie/ytplayer/player/Service.kt
@@ -57,6 +57,7 @@ import org.chromium.net.CronetEngine
 import org.json.JSONArray
 import java.io.File
 import java.text.DecimalFormat
+import java.util.Locale
 import java.util.concurrent.TimeUnit
 
 @OptIn(UnstableApi::class)
@@ -86,6 +87,7 @@ class Service: MediaLibraryService(), MediaLibraryService.MediaLibrarySession.Ca
         val trackSelector: DefaultTrackSelector = DefaultTrackSelector(this).apply {
             setParameters(buildUponParameters()
                 .setForceHighestSupportedBitrate(true)
+                .setPreferredAudioLanguages("${Locale.getDefault().toLanguageTag()},${Locale.getDefault().language}")
             )
         }
 
