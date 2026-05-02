b2bab9c2e977fc34962fc8f04e4adf044261512e
Lillie <91358136+LillieH1000@users.noreply.github.com>
2026-05-02 00:12:40 -0400

    Add downloader class with okhttp for newpipe extractor

---
+++ app/src/main/java/h/lillie/ytplayer/requests/Downloader.kt
@@ -0,0 +1,36 @@
+package h.lillie.ytplayer.requests
+
+import okhttp3.OkHttpClient
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.schabi.newpipe.extractor.downloader.Downloader
+import org.schabi.newpipe.extractor.downloader.Request
+import org.schabi.newpipe.extractor.downloader.Response
+
+class Downloader: Downloader() {
+    override fun execute(request: Request): Response? {
+        val client: OkHttpClient = OkHttpClient.Builder().build()
+
+        val requestBuilder: okhttp3.Request.Builder = okhttp3.Request.Builder()
+            .method(request.httpMethod(), request.dataToSend()?.toRequestBody())
+            .url(request.url())
+
+        request.headers().forEach { (key, values) ->
+            values.forEach { value ->
+                requestBuilder.addHeader(key, value)
+            }
+        }
+
+        val response: okhttp3.Response = client.newCall(requestBuilder.build()).execute()
+        if (!response.isSuccessful) {
+            return null
+        }
+
+        return Response(
+            response.code,
+            response.message,
+            response.headers.toMultimap(),
+            response.body.string(),
+            response.request.url.toString()
+        )
+    }
+}
