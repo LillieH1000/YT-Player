f0cbb82e8ccf6f2fffff0399a0a00f5d621bd9a5
Lillie <91358136+LillieH1000@users.noreply.github.com>
2026-05-02 14:07:19 -0400

    Add media3 dash depends

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,6 +90,7 @@ dependencies {
     implementation(libs.androidx.media3.datasource)
     implementation(libs.androidx.media3.datasource.okhttp)
     implementation(libs.androidx.media3.exoplayer)
+    implementation(libs.androidx.media3.exoplayer.dash)
     implementation(libs.androidx.media3.exoplayer.hls)
     implementation(libs.androidx.media3.session)
     implementation(libs.androidx.media3.ui)
