527bd2d6c26d79be2898a9b5284e3096e8c2b68d
Lillie <91358136+LillieH1000@users.noreply.github.com>
2026-05-18 16:23:59 -0400

    Bump version code for a studio

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "h.lillie.ytplayer"
         minSdk = 30
         targetSdk = 37
-        versionCode = 137
+        versionCode = 138
         versionName = "4.1.1"
     }
 
