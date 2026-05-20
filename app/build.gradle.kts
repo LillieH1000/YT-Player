5dc1a02d17ca5cdc7971d8cda1632923778dcc6a
Lillie <91358136+LillieH1000@users.noreply.github.com>
2026-05-20 15:18:14 -0400

    Update version code

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "h.lillie.ytplayer"
         minSdk = 30
         targetSdk = 37
-        versionCode = 139
+        versionCode = 140
         versionName = "4.1.1"
     }
 
