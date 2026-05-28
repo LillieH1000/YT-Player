0d3a1a54f195355ebcb18d6a024e22d8a686ff0b
Lillie <91358136+LillieH1000@users.noreply.github.com>
2026-05-28 14:00:54 -0400

    Add old python script

---
+++ app/src/main/python/ytdlp.py
@@ -0,0 +1,64 @@
+import json
+import re
+import yt_dlp
+
+def getInfo(videoID, searchQuery):
+    ytdlp_opts = {
+        "extractor_args": {
+            "youtube": {
+                "formats": ["duplicate","missing_pot"],
+                "player_client": ["ios","web_safari"]
+            }
+        },
+        "format": "bestvideo[protocol=m3u8_native]+best[protocol=m3u8_native]/best[protocol=m3u8_native]",
+        "allow_multiple_audio_streams": True,
+        "allow_multiple_video_streams": True,
+        "check_formats": "selected",
+        "noplaylist": True,
+        "cachedir": False
+    }
+
+    info = {}
+    with yt_dlp.YoutubeDL(ytdlp_opts) as ytdlp:
+        if (searchQuery == None):
+            x = ytdlp.extract_info(f"https://www.youtube.com/watch?v={videoID}", download=False)
+            y = json.loads(json.dumps(ytdlp.sanitize_info(x)))
+        if (videoID == None):
+            x = ytdlp.extract_info(f"ytsearch:{searchQuery}", download=False)
+            z = json.loads(json.dumps(ytdlp.sanitize_info(x)))
+            y = z["entries"][0]
+
+        info["id"] = y["id"]
+        info["title"] = y["title"]
+        info["author"] = y["uploader"]
+        info["artwork"] = y["thumbnail"]
+        info["live"] = y["is_live"]
+        info["views"] = y["view_count"]
+        info["likes"] = y["like_count"]
+        info["type"] = y["media_type"]
+        if ("requested_formats" in y):
+            info["iosurl"] = y["requested_formats"][0]["manifest_url"]
+            info["safariurl"] = y["requested_formats"][1]["manifest_url"]
+            info["agent"] = y["requested_formats"][0]["http_headers"]["User-Agent"]
+            info["expiration"] = re.search("/expire/(\\d+)/", y["requested_formats"][0]["manifest_url"]).group(1)
+        else:
+            info["iosurl"] = None
+            info["safariurl"] = y["manifest_url"]
+            info["agent"] = y["http_headers"]["User-Agent"]
+            info["expiration"] = re.search("/expire/(\\d+)/", y["manifest_url"]).group(1)
+        subtitles = []
+        for a in y["subtitles"]:
+            c = {}
+            for b in y["subtitles"][a]:
+                if (b["ext"] == "vtt"):
+                    c["id"] = a
+                    c["name"] = b["name"]
+                    c["url"] = b["url"]
+            if (len(c) != 0):
+                subtitles.append(c)
+        if (len(subtitles) == 0):
+            info["subtitles"] = None
+        else:
+            info["subtitles"] = subtitles
+        
+    return json.dumps(info)
