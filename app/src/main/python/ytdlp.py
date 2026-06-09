2aa779759b20e539de0f918c00674406638fe16c
Lillie <91358136+LillieH1000@users.noreply.github.com>
2026-06-08 22:59:02 -0400

    Use yt-dlp manifest url for live streams hls

--- app/src/main/python/ytdlp.py
+++ app/src/main/python/ytdlp.py
@@ -64,7 +64,7 @@ def getInfo(runtime, videoID, searchQuery):
         # info["expiration"] = re.search("(?:/expire/|[?]expire=)(\\d+)", y["requested_formats"][0]["url"] if "requested_formats" in y else y["url"]).group(1)
         info["expiration"] = "10000000000"
         info["duration"] = y.get("duration", None)
-        info["hlsUrl"] = y.get("url", None)
+        info["hlsUrl"] = y.get("manifest_url", None)
 
         video = []
         for g in y["formats"]:
