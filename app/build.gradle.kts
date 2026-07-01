68ce9ebc429d3b494f43d8fade96e4b2c249b3c4
Lillie <91358136+LillieH1000@users.noreply.github.com>
2026-07-01 14:33:39 -0400

    Update depends and remove unneeded depend

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,7 +85,6 @@ dependencies {
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.foundation)
-    implementation(libs.androidx.lifecycle.common)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.material.icons.core)
     implementation(libs.androidx.material.icons.extended)
